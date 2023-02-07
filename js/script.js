ab6cbebea8228fdd6bb42faf86c09ecae73b7127
oliabully <myau91@gmail.com>
2023-02-07 15:44:01 +0400

    adding my files to my repository

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let loginButton = document.querySelector('.login_button');
+let loginPopUp = document.querySelector('.login_popup');
+let closeButton = document.querySelector('.close_button');
+
+loginButton.addEventListener('click', showPopUp);
+
+closeButton.addEventListener('click', showPopUp);
+
+function showPopUp(evt) {
+    console.log(evt);
+    evt.preventDefault();
+    loginPopUp.classList.toggle('show_popup');
+}
